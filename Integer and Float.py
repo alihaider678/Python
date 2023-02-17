c0f715048c1846be50fc77b28d93b54b17e65747
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2023-02-17 23:58:20 +0500

    Arithmetic & Comparison
    Some basic math

---
+++ Integer and Float.py
@@ -0,0 +1,61 @@
+#What is the type of number?
+num = 3.14
+print(type(num))
+
+# SOME BASIC MATHEMATICS
+# Arithmetic Operators
+# Addition:          	   3+2
+# Subtraction:       	   3-2
+# Multiplication:          3*2
+# Division:    		       3/2
+# Floor Division:          3//2
+# Exponenet:               3**2
+# Modolus:                 3%2
+
+
+# Comparisons:
+# Equal:          	              3==2
+# Not Equal:       	              3!=2
+# Greater than:                   3>2
+# Less than:    		          3<2
+# Greater or equal to:            3>=2
+# Less or equal to:               3<=2
+
+print(3//2)
+print(3 * 6 - 5)
+
+num =1 
+
+num += 1
+
+print(num)
+
+#print absolute value
+
+print(abs(-8))
+
+#print round value
+
+print(round(5.6))
+
+
+#comparison
+
+num1 = 2
+num2 = 3
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+
+
+
+
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
