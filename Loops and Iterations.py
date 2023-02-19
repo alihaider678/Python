025b771fb7b5ef783aad5856d85750d152294e00
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2023-02-19 21:25:11 +0500

    For/While Loops
    For loop, nested loop and while loop

---
+++ Loops and Iterations.py
@@ -0,0 +1,44 @@
+# Intro
+
+# nums = [1 , 2, 3, 4, 5]
+# for num in nums:
+# 	print(num)
+
+#For Loop
+
+nums = [1 , 2, 3, 4, 5]
+for num in nums:
+	if num == 3:
+		print('Found!')
+		break
+print(num)
+
+#Nested Loop
+for num in nums:
+	for letter in 'abc':
+		print(num, letter)
+
+
+for i in range(10):
+	print(i)
+
+
+for i in range(1 , 11):
+	print(i)
+
+
+# While Loop
+
+x = 0
+
+while x < 10:
+	print(x)
+	x += 1
+
+x = 0
+
+while x < 10:
+	if x == 5:
+		break
+	print(x)
+	x += 1
