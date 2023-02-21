6931a8a649d7d2b0215303ecd60c5961ae554024
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2023-02-21 10:32:33 +0500

    Classes and Methods
    Intro, classes, methods

---
+++ Python OOP/Classes and Instances.py
@@ -0,0 +1,22 @@
+# Pyhton Object Oriented Programming
+
+# 1) Intro
+
+# 2) Classes
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+emp_1.fullname()
+print(Employee.fullname(emp_1))
+
